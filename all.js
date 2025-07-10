a4984b43a66162e0163307089a4cd3d69ec573dd
luchen <luchen5@luchendeMacBook-Air.local>
2025-07-10 17:58:51 +0800

    1014

--- all.js
+++ all.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         基座模型-工时填写助手
 // @namespace    li-auto-jizuomoxing-luchen
-// @version      1.0.13
+// @version      1.0.14
 // @description  工时一键上报
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
@@ -17,6 +17,8 @@
 ⭐️ greasyfork 作为安装源(镜像)，后续更新有问题(镜像非镜像均不行，因为greasyfork分配的downloadURL无法直接访问)，解决方式如下：
  方式1：安装后，使用github raw.githubusercontent.com 作为更新源即可，但是需要用户指定地址
  方式2：用户点击检查更新，跳转到镜像站，镜像站会自动更新，但是需要重新安装
+ =>简化方式：用户点击检查更新，跳转到镜像站，镜像站会自动更新，但是需要重新安装
+ 应该不会有大更新了，有问题就重新安装吧
 */
 /*! jQuery v3.7.1 | (c) OpenJS Foundation and other contributors | jquery.org/license */
 
